7e6eb3bdc2ee419df4552643cf3a5432d424a311
suraj <2024mt03557@wilp.bits-pilani.ac.in>
2025-10-21 22:41:06 +0800

    single flow for data and ml

---
+++ master_pipeline.py
@@ -0,0 +1,11 @@
+from prefect import flow
+from data_pipeline_prefect import data_pipeline_flow
+from ml_pipeline_prefect import ml_pipeline_flow
+
+@flow(name="Master Farmer Pipeline")
+def master_pipeline_flow():
+    data_pipeline_flow()   # Run data ingestion + preprocessing
+    ml_pipeline_flow()     # Then run ML training + evaluation
+
+if __name__ == "__main__":
+    master_pipeline_flow()
