ef7d6bd07c35a9c304f3ba32812b873493ece99d
suraj <2024mt03557@wilp.bits-pilani.ac.in>
2025-10-25 22:08:28 +0800

    after adding the get run logger()

--- ml_pipeline_prefect.py
+++ ml_pipeline_prefect.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, precision_score, recall_score, f1_score
+from prefect.logging import get_run_logger
 import pickle
 import logging
 import numpy as np
@@ -35,19 +36,20 @@ def train_models(data):
 
 @task
 def evaluate_models(reg_model, clf_model, X_test_r, y_test_r, X_test_c, y_test_c):
-    logging.info("Evaluating models...")
+    logger = get_run_logger()
+    logger.info("Evaluating models...")
     y_pred_r = reg_model.predict(X_test_r)
     #logging.info(f"Regression RMSE: {mean_squared_error(y_test_r, y_pred_r, squared=False):.2f}, R2: {r2_score(y_test_r, y_pred_r):.2f}")
     rmse = np.sqrt(mean_squared_error(y_test_r, y_pred_r))  # safer than squared=False
     r2 = r2_score(y_test_r, y_pred_r)
-    logging.info(f"Regression RMSE: {rmse:.2f}, R2: {r2:.2f}")
+    logger.info(f"Regression RMSE: {rmse:.2f}, R2: {r2:.2f}")
 
     y_pred_c = clf_model.predict(X_test_c)
     acc = accuracy_score(y_test_c, y_pred_c)
     precision = precision_score(y_test_c, y_pred_c)
     recall = recall_score(y_test_c, y_pred_c)
     f1 = f1_score(y_test_c, y_pred_c)
-    logging.info(f"Classification metrics - Acc: {acc:.3f}, Precision: {precision:.3f}, Recall: {recall:.3f}, F1: {f1:.3f}")
+    logger.info(f"Classification metrics - Acc: {acc:.3f}, Precision: {precision:.3f}, Recall: {recall:.3f}, F1: {f1:.3f}")
     
 @task
 def save_models(reg_model, clf_model):
